a8f2dfbb3c65c056d352638fa6028d7055fc6067
sneyder98 <31749034+sneyder98@users.noreply.github.com>
2017-09-30 20:29:24 -0500

    Primer Commit Sneyder Bravo
    Aqui cree el menu de la app e inclui el primer modelo 3D de la entrada y empezamos el proyecto del GPS en fase beta aun

---
+++ Assets/Scripts/RotateScenario.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScenario : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(x: 0, y: 1.0f, z: 0));
+	}
+}
